e8fdde0dba2b9e396953c64547e02b3aa1abb9c2
syedaalaina <146921862+syedaalaina@users.noreply.github.com>
2025-04-25 00:09:58 -0700

    Update page.tsx

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -9,7 +9,7 @@ import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
 import React, { useState } from 'react'
 
 const Meeting = ({ params: { id } }: { params: { id: string } }) => {
-  const { isLoaded, user } = useUser();
+  const { isLoaded } = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false)
   const { call, isCallLoading } = useGetCallById(id);
 
